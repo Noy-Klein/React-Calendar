0c6bac0d3b8c2b1320309a77b98233f885e2011e
nuzzi123 <nuzzi123@gmail.com>
2018-12-14 14:25:16 +0200

    Updates

---
+++ precache-manifest.588429be98cbae07c20c35384c9e7b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a29fbc241201b8d1bc7",
+    "url": "/React-Calendar/static/js/runtime~main.0a29fbc2.js"
+  },
+  {
+    "revision": "23fa5b22c8ab46c90df9",
+    "url": "/React-Calendar/static/js/main.23fa5b22.chunk.js"
+  },
+  {
+    "revision": "a27a4eac5e35bd690f75",
+    "url": "/React-Calendar/static/js/1.a27a4eac.chunk.js"
+  },
+  {
+    "revision": "23fa5b22c8ab46c90df9",
+    "url": "/React-Calendar/static/css/main.a801f4c9.chunk.css"
+  },
+  {
+    "revision": "a27a4eac5e35bd690f75",
+    "url": "/React-Calendar/static/css/1.e15a58ec.chunk.css"
+  },
+  {
+    "revision": "84d3bc227ca5694bc77d58d237564bdd",
+    "url": "/React-Calendar/index.html"
+  }
+];
